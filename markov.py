bbb98a7f9b96ca5a0ac596271ff12368fc80b155
Hackbright Student <info@hackbrightacademy.com>
2016-07-13 13:00:44 -0700

    Abbreviated code to add values to dictionary. Started working on n-gram code.

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 from random import choice
 
+import sys
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
@@ -16,7 +17,7 @@ def open_and_read_file(file_path):
 
     return whole_file
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -30,41 +31,58 @@ def make_chains(text_string):
     """
 
     dict_chains = {}
+    keys = []
 
     #  Split the words by spaces in the file that is now a string.
     indiv_words = text_string.split()
 
     #  For every value in the string, up to and including the last word.
-    for num in range(0, len(indiv_words)- 2):
+    for num in range(0, len(indiv_words)- n):
 
-        # The word at the current index becomes the first word in the key.
-        first_key = indiv_words[num]      
+        for x in range(num,num+n):
+            keys.append(indiv_words[x])
 
-        # The word at the following index becomes the second word in the key.      
-        second_key = indiv_words[num + 1]
+    print keys
 
-        # The word following second_key will be called third_word.
-        third_word = indiv_words[num + 2]
+
+        # # The word at the current index becomes the first word in the key.
+        # first_key = indiv_words[num]      
+
+        # # The word at the following index becomes the second word in the key.      
+        # second_key = indiv_words[num + 1]
+
+        # # The word following second_key will be called third_word.
+        # third_word = indiv_words[num + 2]
 
         # We will make a key using the first and second words grabbed.
-        tuple_key = (first_key, second_key)
+        # tuple_key = (first_key, second_key)
+        #Ahm,ad's code
+        #for x in range(num,num+2):
+        #     list.append(words[x])
 
-        # If the tuple key is not already in our dictionary,
-        if tuple_key not in dict_chains:
+    #     # Check to see if the dictionary contains the key. If not, return [] 
+    #     #and add the empty list as the value associated with the key.
+    #     dict_chains[tuple_key] = dict_chains.get(tuple_key, [])
 
-            # Our current empty dictionary
-            dict_chains[tuple_key] = []
+    #     # Append the value 'third_word' to the value
+    #     dict_chains[tuple_key].append(third_word)
+        
+    #     # If the tuple key is not already in our dictionary,
+    #     # if tuple_key not in dict_chains:
 
-            # Add the key to and the value (third_word) to the dictionary.
-            dict_chains[tuple_key].append(third_word)
+    #     #     # Our current empty dictionary
+    #     #     dict_chains[tuple_key] = []
 
-        # If the key is already in the dictionary, add the third_word to its 
-        # value.    
-        else:
-            dict_chains[tuple_key].append(third_word)
+    #     #     # Add the key to and the value (third_word) to the dictionary.
+    #     #     dict_chains[tuple_key].append(third_word)
 
-    # Return our dictionary.
-    return dict_chains
+    #     # # If the key is already in the dictionary, add the third_word to its 
+    #     # # value.    
+    #     # else:
+    #     #     dict_chains[tuple_key].append(third_word)
+
+    # # Return our dictionary.
+    # return dict_chains
    
 
 def make_text(chains):
@@ -101,13 +119,13 @@ def make_text(chains):
 
 
 # Put the file that you want to apply Markov Magic to.
-input_path = "green-eggs.txt"
+input_path = 'green-eggs.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, 3)
 
 # Produce random text
 random_text = make_text(chains)
