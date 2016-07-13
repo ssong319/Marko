79bd5d50e7cf062e78639624ee4285759ad4e34c
Hackbright Student <info@hackbrightacademy.com>
2016-07-12 18:07:53 -0700

    Add comments to all of the functions and stylize the script.

--- markov.py
+++ markov.py
@@ -8,7 +8,10 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
+    # Open the file.
     open_file = open(file_path)
+
+    # Turn the file into one long string.
     whole_file = open_file.read()
 
     return whole_file
@@ -28,52 +31,76 @@ def make_chains(text_string):
 
     dict_chains = {}
 
-
-
+    #  Split the words by spaces in the file that is now a string.
     indiv_words = text_string.split()
 
+    #  For every value in the string, up to and including the last word.
     for num in range(0, len(indiv_words)- 2):
-        first_key = indiv_words[num]
+
+        # The word at the current index becomes the first word in the key.
+        first_key = indiv_words[num]      
+
+        # The word at the following index becomes the second word in the key.      
         second_key = indiv_words[num + 1]
+
+        # The word following second_key will be called third_word.
         third_word = indiv_words[num + 2]
+
+        # We will make a key using the first and second words grabbed.
         tuple_key = (first_key, second_key)
+
+        # If the tuple key is not already in our dictionary,
         if tuple_key not in dict_chains:
+
+            # Our current empty dictionary
             dict_chains[tuple_key] = []
+
+            # Add the key to and the value (third_word) to the dictionary.
             dict_chains[tuple_key].append(third_word)
+
+        # If the key is already in the dictionary, add the third_word to its 
+        # value.    
         else:
             dict_chains[tuple_key].append(third_word)
 
+    # Return our dictionary.
     return dict_chains
-    
-
-    
-
    
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    list_of_keys = chains.keys()        # Create a variable that lists all the keys. 
+    # Create a variable that lists all the keys.
+    list_of_keys = chains.keys()         
 
-    random_key = choice(list_of_keys)   # Create variable that generates one random key.
+    # Create variable that generates one random key.
+    random_key = choice(list_of_keys)   
 
+    # Initialize our text with a single random key.
     text = random_key[0]
 
+    # While the key exists in our dictionary,
     while random_key in chains:
+
+        # Add it to our text.
         text = text + " " + random_key[1]
 
-        list_associated_with_key = chains[random_key] #this var references the list associated
-                                                        #with the random_key
-        word = choice(list_associated_with_key)     #word is a random word from the list
+        # Grab the list of values associated with the random_key.
+        list_associated_with_key = chains[random_key]
+                                  
+        # Grab a random word from that list.                                               
+        word = choice(list_associated_with_key)     
 
-        random_key = (random_key[1], word)         # Create a key out of the second word in the first
+        # Set a new random key using the second word in the key and the word
+        # that was just used.
+        random_key = (random_key[1], word)         
     
-                                            # key and a random word from the list of words.
+    # Show us the text.
     return text
 
 
 
-
+# Put the file that you want to apply Markov Magic to.
 input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
@@ -81,8 +108,6 @@ input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
 chains = make_chains(input_text)
-
-#print chains
 
 # Produce random text
 random_text = make_text(chains)
